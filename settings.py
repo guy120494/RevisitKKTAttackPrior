f69caf55dc387fc6d20250850b06239fee9f910e
Guy Smorodinsky <guy120494@gmail.com>
2025-07-15 15:48:16 +0300

    Changed path in slurm server

---
+++ settings.py
@@ -0,0 +1,7 @@
+# datasets_dir = r'C:\Users\admin\PycharmProjects\RevisitingKKTAttackPrior\datasets'
+# results_base_dir = r'C:\Users\admin\PycharmProjects\RevisitingKKTAttackPrior\results'
+# models_dir = r'C:\Users\admin\PycharmProjects\RevisitingKKTAttackPrior\models'
+
+datasets_dir = r'/home/smorodin/revisitKKTAttack/datasets'
+results_base_dir = r'/home/smorodin/revisitKKTAttack/results'
+models_dir = r'/home/smorodin/revisitKKTAttack/models'
